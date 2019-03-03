93a25207c2da2c237df4b413aca16b88a89bc0ac
Gunslinger <jtgeislinger@gmail.com>
2019-03-02 23:41:25 -0500

    update JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,17 @@ const mysql = require("mysql");
 // ====================================================
 // Set Up MySql Connection
 // ====================================================
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burger_db"
+    });
+};
 
 // ====================================================
 // Make Connection
